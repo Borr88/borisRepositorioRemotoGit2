8921bbdb7abb48df6e617da8eb9b610b69300a8a
borr1 <borr1@Portatil-Boris>
2024-04-15 15:14:39 +0200

    prueba2 rama1 proyecto2 mensajes 2 y 3

--- src/rama1Proyecto2/rama1Proyecto2pureba.java
+++ src/rama1Proyecto2/rama1Proyecto2pureba.java
@@ -3,8 +3,9 @@ package rama1Proyecto2;
 public class rama1Proyecto2pureba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Mensaje prueba1 rama1 proyecto2");
+		System.out.println("Mensaje 2 prueba1 rama1 proyecto2");
+		System.out.println("Mensaje 3 prueba1 rama1 proyecto2");
 	}
 
 }

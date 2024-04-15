2bdcab6643bb7c79ad8bb458bd1d1b227e0b37f2
borr1 <borr1@Portatil-Boris>
2024-04-15 14:07:29 +0200

    rama1 proyecto2

--- src/Proyecto2/ProyectoGit.java
+++ src/Proyecto2/ProyectoGit.java
@@ -3,8 +3,7 @@ package Proyecto2;
 public class ProyectoGit {
 
 	public static void main(String[] args) {
-		System.out.println("Mensaje practica GIT");
-
+		System.out.println("Mensaje practica GIT Ejercicio1");
 	}
 
 }

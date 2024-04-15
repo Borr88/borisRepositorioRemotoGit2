079208e9dc7b9272a238581c6c573a01b7e80501
borr1 <borr1@Portatil-Boris>
2024-04-15 14:16:48 +0200

    Rama1 proyecto2 mensajes 2 y 3

---
+++ src/ProyectoRama/Rama1Proyecto.java
@@ -0,0 +1,11 @@
+package ProyectoRama;
+
+public class Rama1Proyecto {
+
+	public static void main(String[] args) {
+		System.out.println("Rama 1 proyecto 2");		
+		System.out.println("Mensaje 2");
+		System.out.println("Mensaje 3");
+	}
+
+}
